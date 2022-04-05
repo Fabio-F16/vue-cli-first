63273927df18f970c0f65d76735c012ec74b3e89
Fabio-F16 <fipsfabio95@gmail.com>
2022-04-05 16:09:45 +0200

    esercizio completato..?

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <MainTitle />
+    <HeroJumbotron />
+  </div>
+</template>
+
+<script>
+import MainTitle from "./components/MainTitle.vue";
+import HeroJumbotron from "./components/HeroJumbotron.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainTitle,
+    HeroJumbotron,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeroJumbotron.vue
@@ -0,0 +1,6 @@
+<template>
+  <img
+    src="https://media.geeksforgeeks.org/wp-content/uploads/20190501153200/jumbotron1.png"
+    alt=""
+  />
+</template>
